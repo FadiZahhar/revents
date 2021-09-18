327008ac08cb564d6e81b5c2f87110d5b1a1710a
Apple <apple@Apples-MacBook-Pro.local>
2021-09-18 12:25:50 +0300

    adding the changes

---
+++ src/features/nav/NavBar.jsx
@@ -0,0 +1,13 @@
+import react from 'react';
+import { Menu } from 'semantic-ui-react';
+
+export default function NavBar() {
+    <Menu inverted fixed='top'>
+        <Container>
+            <Menu.Item header>
+                <img src="/assets/logo.png" alt="logo" />
+                Re-vents
+            </Menu.Item>
+        </Container>
+    </Menu>
+}
