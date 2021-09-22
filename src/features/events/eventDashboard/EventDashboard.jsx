395c1df6fbd78e01a0bd1d2069b05fcf96d269fe
Fadi Zahhar <79freelancer@gmail.com>
2021-09-22 20:27:50 +0300

    finishing section 3 components and props

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {sampleData} from '../../../app/api/sampleData.js';
 import {Grid} from 'semantic-ui-react';
 import EventList from './EventList';
@@ -6,14 +6,18 @@ import EventForm from '../eventForm/EventForm';
 
 
 // array functions
-const EventDashboard  = ()  => {
+const EventDashboard  = ({formOpen,setFormOpen})  => {
+    const {events,setEvents} = useState(sampleData);
+    
+
     return (
         <Grid>
             <Grid.Column width={10}>
                 <EventList events={sampleData} />
             </Grid.Column>
             <Grid.Column width={6}>
-                <EventForm />
+                {formOpen &&
+                <EventForm setFormOpen={setFormOpen} />}
             </Grid.Column>
         </Grid>
     )
