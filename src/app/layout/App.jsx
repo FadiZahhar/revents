4aaf40666351c30abe95b048dd130816c7d00412
Fadi Zahhar <79freelancer@gmail.com>
2021-10-03 13:32:11 +0300

    finishing the navigation

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,7 +1,11 @@
 import React, {useState} from 'react';
 import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
+import HomePage from '../../features/home/HomePage';
+import EventForm from '../../features/events/eventForm/EventForm';
+import EventDetails from '../../features/events/eventDetailed/EventDetailedPage';
 import NavBar from '../../features/nav/NavBar';
 import {Container} from 'semantic-ui-react';
+import {Route} from 'react-router-dom';
 
 function App() {
   const [formOpen,setFormOpen] = useState(false);
@@ -21,14 +25,11 @@ function App() {
   <>
   <NavBar setFormOpen={handleCreateFormOpen} />
   <Container className="main">
-  <EventDashboard 
-  formOpen={formOpen} 
-  setFormOpen={setFormOpen}
-  selectEvent={handleSelectEvent}
-  selectedEvent={selectedEvent}
-  />
+    <Route exact path="/" component={HomePage} />
+    <Route exact path='/events' component={EventDashboard} /> 
+    <Route path='/events/:id' component={EventDetails} />
+    <Route path='/createEvent' component={EventForm} />
   </Container>
-  
 </>
 );
 }
--- src/features/nav/NavBar.jsx
+++ src/features/nav/NavBar.jsx
@@ -1,18 +1,18 @@
 import React, {useState} from 'react';
 import { Menu,Container,Button } from 'semantic-ui-react';
-
+import { NavLink } from 'react-router-dom';
 export default function NavBar({setFormOpen}) {
 
 
     return (
     <Menu inverted fixed='top'>
         <Container>
-            <Menu.Item header>
+            <Menu.Item as={NavLink} to='/' header>
                 <img src="/assets/logo.png" alt="logo" />
                 Re-vents
             </Menu.Item>
-            <Menu.Item name='Events' />
-            <Menu.Item>
+            <Menu.Item as={NavLink} exact to='events' name='Events' />
+            <Menu.Item as={NavLink} to='/createEvent'>
                 <Button onClick={() => setFormOpen(true)} positive inverted center content="Create Event" />
             </Menu.Item>
             <Menu.Item position="right"> 
