912efb57fcda35af5a064ac539c1dd863345b08e
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2021-10-22 15:54:56 +0300

    adding the firestore service

--- src/app/firestore/firestoreService.js
+++ src/app/firestore/firestoreService.js
@@ -2,6 +2,10 @@ import firebase from '../config/firebase';
 
 const db = firebase.firestore();
 
+export function dataFromSnapshot(snapshot) {
+    if(!snapshot.exists) return undefined;
+    const data = snapshot.data();
+}
 export default function getEventsFromFirestore(observer) {
     return db.collection('events').onSnapshot(observer);
 }
